b6be21ea53b78e2533b5ef504b43f6a6770a48b6
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2023-05-17 13:52:04 -0300

    feat: alter break line format

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,5 @@
-/* eslint-disable prettier/prettier */
 export { Button } from "./Button";
 export { Header } from "./Header";
 export { Loading } from "./Loading";
+// eslint-disable-next-line prettier/prettier
 
