1501a173b22a2b3b7640e73760f737696c2a2d50
Diogines Augusto <dioginisaugusto1@gmail.com>
2023-07-01 02:52:06 -0300

    visualizar parcial

--- src/screens/Chat/Channel.tsx
+++ src/screens/Chat/Channel.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import { ScrollView, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -29,6 +29,7 @@ export const Channel = () => {
   const [messages, setMessages] = useState<BaseMessage[]>([])
   const [channel, setChannel] = useState<GroupChannel>()
   const [isTyping, setIsTyping] = useState(false)
+  const [readReceipts, setReadReceipts] = useState<Record<string, boolean>>({})
 
   const scrollViewRef = useRef<ScrollView>(null)
 
@@ -36,14 +37,22 @@ export const Channel = () => {
 
   const UNIQUE_HANDLER_ID = 'UNIQUE_HANDLER_ID'
 
+  const updateReadReceipts = useCallback(() => {
+    if (!channel) return
+    const newReadReceipts: Record<string, boolean> = {}
+    messages.forEach((message) => {
+      const readStatus = channel.getReadStatus()
+      for (const memberId in readStatus) {
+        if (memberId !== sb.currentUser.userId) {
+          newReadReceipts[message.messageId] =
+            readStatus[memberId].readAt <= message.createdAt
+        }
+      }
+    })
+    setReadReceipts(newReadReceipts)
+  }, [channel, messages])
+
   useEffect(() => {
-    const onFocus = () => {
-      if (!channel) return
-      channel.markAsRead()
-    }
-
-    const unsubscribe = navigation.addListener('focus', onFocus)
-
     async function loadPreviousMessages() {
       try {
         const groupChannel = await sb.groupChannel.getChannel(channelUrl)
@@ -69,18 +78,15 @@ export const Channel = () => {
             onMessageReceived: (channel: BaseChannel, message: BaseMessage) => {
               setMessages((prevMessages) => [...prevMessages, message])
             },
-            onMessageUpdated: (
-              channel: BaseChannel,
-              message: BaseMessage,
-            ) => { },
-            onMessageDeleted: (channel: BaseChannel, messageId: number) => { },
-            onUndeliveredMemberStatusUpdated: (channel: GroupChannel) => { },
-            onUnreadMemberStatusUpdated: (channel: GroupChannel) => {
-              console.log(channel.unreadMessageCount)
-            },
+            onUnreadMemberStatusUpdated: (channel: GroupChannel) => { },
             onTypingStatusUpdated: (channel: GroupChannel) => {
               const typingMembers = channel.getTypingUsers()
               setIsTyping(typingMembers.length > 0)
+            },
+            onChannelChanged: (channel) => {
+              if (channel instanceof GroupChannel) {
+                updateReadReceipts()
+              }
             },
           })
 
@@ -96,9 +102,7 @@ export const Channel = () => {
     }
 
     loadPreviousMessages()
-
-    return unsubscribe
-  }, [channelUrl, channel, navigation, isTyping])
+  }, [channelUrl, channel, navigation, isTyping, messages, updateReadReceipts])
 
   function handleSetMessage(text: string) {
     setMessage(text)
@@ -107,7 +111,6 @@ export const Channel = () => {
         .startTyping()
         .then(() => {
           setIsTyping(true)
-          console.log(isTyping)
         })
         .catch((err) => console.log(err))
         .finally(() => setIsTyping(false))
@@ -126,15 +129,20 @@ export const Channel = () => {
         console.log(err, message)
       })
       .onSucceeded((message: BaseMessage) => {
+        channel.markAsRead()
         setMessages([...messages, message])
         channel.endTyping()
       })
     setMessage('')
   }
 
+  const currentMember = channel?.members.find(
+    (member) => member.userId !== userCred.userId,
+  )
+  const userName = currentMember?.nickname
   return (
     <SafeAreaView className="flex-1">
-      <Header userName={channel?.members[1].nickname!} />
+      <Header userName={userName || ''} />
       <ScrollView
         ref={scrollViewRef}
         className="border-b border-t border-gray-300/50 bg-gray-200 px-4 pt-2"
@@ -146,6 +154,7 @@ export const Channel = () => {
                 key={msg.messageId}
                 data={msg}
                 isSender={userCred.userId === msg.sender.userId}
+                messageRead={readReceipts[msg.messageId]}
               />
             )
           } else {
